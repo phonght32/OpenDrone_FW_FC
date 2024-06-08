312a3d94f44f52b360cf39280d04e44387bdec89
phonght32 <thanhphongho1998@gmail.com>
2024-06-08 21:52:48 +0700

    update description

--- OpenDrone_FC.h
+++ OpenDrone_FC.h
@@ -1,3 +1,32 @@
+// MIT License
+
+// Copyright (c) 2024 phonght32
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef __OPENDRONE_FC_H__
+#define __OPENDRONE_FC_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "Algorithm/pid_controller/pid_controller.h"
 #include "EscProtocol/esc_dshot/esc_dshot.h"
 #include "Filter/imu_madgwick/imu_madgwick.h"
@@ -9,3 +38,9 @@
 #include "Sensor/hmc5883l/hmc5883l.h"
 #include "Sensor/mpu6050/mpu6050.h"
 #include "TxProtocol/OpenDrone_TxProto/OpenDrone_TxProto.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __OPENDRONE_FC_H__ */
